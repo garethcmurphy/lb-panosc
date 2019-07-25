5111fee542f70990959f0307ed016893f4a387fc
Gareth Murphy <garethcmurphy@gmail.com>
2019-07-25 15:44:46 +0200

    add query for panosc

--- common/models/dataset.js
+++ common/models/dataset.js
@@ -1,4 +1,43 @@
 "use strict";
+var requestPromise = require("request-promise");
+
+function stringEncode(fields) {
+  const fieldString = JSON.stringify(fields);
+  return encodeURIComponent(fieldString);
+}
+
+function buildURI(essURI, fields, limits) {
+  const encodedFields = stringEncode(fields);
+  const encodedLimits = stringEncode(limits);
+  const finalURI =
+    essURI + "?fields" + encodedFields + "&limits=" + encodedLimits;
+  return finalURI;
+}
+
+async function CERICrequest(queryObject) {
+  const results = [];
+  return results;
+}
+
+async function ESSrequest(queryObject) {
+  var results;
+  const essURI = "https://scicatapi.esss.dk/api/v3/Datasets/fullquery";
+  const fields = queryObject; 
+  const limits = { limit: "3", order: "size ASC" };
+  const finalURI = buildURI(essURI, fields, limits);
+  var searchOptions = {
+    method: "GET",
+    uri: finalURI
+  };
+  try {
+    const response = await requestPromise(searchOptions);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
+  return results;
+}
 
 module.exports = function(Dataset) {
   /**
@@ -8,11 +47,13 @@ module.exports = function(Dataset) {
    */
 
   Dataset.query = function(searchTerm, callback) {
-    let queryResults;
-    // TODO
-    // read institutes from file or fetch from mongodb
+    var queryResults;
 
     // send request to six institutes
+    const cericResults = CERICrequest(searchTerm);
+    queryResults.concat(cericResults);
+    const essResults = ESSrequest(searchTerm);
+    queryResults.concat(essResults);
 
     // aggregate results and return to user
 

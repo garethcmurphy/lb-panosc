9a513716e187f2bbd73696a93fd63314c2b455fd
Gareth Murphy <garethcmurphy@gmail.com>
2019-07-25 14:10:43 +0200

    update to add query for different institutes

--- common/models/dataset.js
+++ common/models/dataset.js
@@ -1,5 +1,21 @@
-'use strict';
+"use strict";
 
 module.exports = function(Dataset) {
+  /**
+   * Query PaNOSC institutes for scientific metadata
+   * @param {object} searchTerm Object containing one or more properties, e.g. wavelength, or text for full text search
+   * @param {Function(Error, object)} callback
+   */
 
+  Dataset.query = function(searchTerm, callback) {
+    let queryResults;
+    // TODO
+    // read institutes from file or fetch from mongodb
+
+    // send request to six institutes
+
+    // aggregate results and return to user
+
+    callback(null, queryResults);
+  };
 };
